707efbbfb16ad5d927e95df09bcfee7a43067f76
talhagozutok <96948081+talhagozutok@users.noreply.github.com>
2024-10-14 01:32:46 +0300

    Add filters widget

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -10,20 +10,67 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Todo App'),
       ),
-      body: const Center(
-        child: Column(
-          children: [
-            TaskListTile(
-              title: 'Task title',
-              description: 'No description',
-              date: '2024-10-13',
-              isCompleted: false,
+      body: const Column(
+        children: [
+          FiltersWidget(),
+          Divider(),
+          Center(
+            child: Column(
+              children: [
+                TaskListTile(
+                  title: 'Task title',
+                  description: 'No description',
+                  date: '2024-10-13',
+                  isCompleted: false,
+                ),
+                TaskListTile(
+                  title: 'Make the mistake you had to make.',
+                  description: 'No description',
+                  date: '2024-10-13',
+                  isCompleted: true,
+                ),
+              ],
             ),
-            TaskListTile(
-              title: 'Make the mistake you had to make.',
-              description: 'No description',
-              date: '2024-10-13',
-              isCompleted: true,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class FiltersWidget extends StatelessWidget {
+  const FiltersWidget({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8, right: 8),
+        child: Row(
+          children: [
+            InputChip(
+              label: const Text('All'),
+              selected: true,
+              onSelected: (_) => {},
+              selectedColor: true ? Colors.lightGreen : null,
+            ),
+            const SizedBox(width: 8),
+            InputChip(
+              label: const Text('Completed'),
+              selected: false,
+              onSelected: (_) => {},
+              selectedColor: false ? Colors.lightGreen : null,
+            ),
+            const SizedBox(width: 8),
+            InputChip(
+              label: const Text('Incomplete'),
+              selected: false,
+              onSelected: (_) => {},
+              selectedColor: false ? Colors.lightGreen : null,
             ),
           ],
         ),
